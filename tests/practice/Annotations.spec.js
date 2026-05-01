67a6425d75c47403056b334a1df042b4fd860520
AnnadTenneti <tenneti.anand@gmail.com>
2026-05-01 10:31:11 +0530

    Remove practice folder from GitHub

--- tests/practice/Annotations.spec.js
+++
@@ -1,5 +0,0 @@
-import {test} from '@playwright/test';
-
-test.fixme('This test is skipped because it is not ready yet', async () => {
- console.log('This test will be skipped');
-});
--- tests/practice/test_fixtures.spec.js
+++
@@ -1,7 +0,0 @@
-import {test, expect} from "./fixture"
-
-test('Verify login fixture', async ({login}) => {
-    console.log('This test is using the login fixture');
-    await expect(login).toHaveURL('/ecommerce/login');
-    await login.waitForTimeout(2000);
-});
