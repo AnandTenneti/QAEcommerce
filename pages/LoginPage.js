2840d06e4a080d3f68ccd0a7388569f82d0a3168
AnnadTenneti <tenneti.anand@gmail.com>
2026-05-01 23:55:26 +0530

    Updating LoginPage

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,21 +1,28 @@
 export class LoginPage {
-    constructor(page) {
-        this.page = page;
-        this.usernameInput = page.getByRole('textbox', { name: 'email' });
-        this.passwordInput = page.getByLabel('Password', { name: 'password' });
-        this.loginButton = page.locator("//button[@type='submit' and text()='Login']");
-    }
+  constructor(page) {
+    this.page = page;
+    this.usernameInput = page.getByRole("textbox", { name: "email" });
+    this.passwordInput = page.getByLabel("Password", { name: "password" });
+    this.loginButton = page.locator("//button[@type='submit' and text()='Login']",
+    );
+    this.errorMessages = page.locator("div[data-title]");
+  }
 
-    async goToApplicationLoginPage() {
-        await this.page.goto('/ecommerce/login');
-    }
+  async goToApplicationLoginPage() {
+    await this.page.goto("/ecommerce/login");
+  }
 
-    async login(username, password) {
-        await this.usernameInput.fill(username);
-        await this.passwordInput.fill(password);
-        await Promise.all([
-            this.page.waitForURL('/ecommerce'),
-            this.loginButton.click(),
-        ]);
-    }
+  async login(username, password) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+ async getErrorMessages() {
+    await this.page.waitForSelector("div[data-title]", { timeout: 5000 });
+  return await this.errorMessages.allTextContents();
+}
+
+ 
+ 
+  
 }
