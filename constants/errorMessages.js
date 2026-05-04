52623265bb762d59775a862a60e9f3a22d061305
AnnadTenneti <tenneti.anand@gmail.com>
2026-05-04 15:40:55 +0530

    Adding errorMessages file under constants

---
+++ constants/errorMessages.js
@@ -0,0 +1,5 @@
+export const errorMessages = {
+  invalidPassword: "Username matched but password is incorrect.",
+  invalidUsername: "Password matched but email is incorrect.",
+  invalidUsernameAndPassword: "Neither email nor password matched.",
+};
