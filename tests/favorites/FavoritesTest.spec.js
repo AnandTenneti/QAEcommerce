573493c5bd88cea5db24c74a356461ed1a00a7fd
AnnadTenneti <tenneti.anand@gmail.com>
2026-05-04 22:59:19 +0530

    Refactoring tests

---
+++ tests/favorites/FavoritesTest.spec.js
@@ -0,0 +1,34 @@
+import { test, expect } from "@fixtures/fixtures";
+test.afterEach(async ({ headerPage }) => {
+  await headerPage.logout();
+});
+
+test("[@regression] Verify empty favorites state and product addition behavior", async ({
+  loggedInPage,
+  homePage,
+  headerPage,
+  favoritesPage,
+}) => {
+  await test.step("Navigate to Favorites and verify empty state", async () => {
+    await headerPage.clickOnFavorite();
+    await expect(loggedInPage).toHaveURL(/\/favorites$/);
+    await expect(favoritesPage.getMessage()).toContainText(
+      "You have no favorite products",
+    );
+  });
+
+  await test.step("Navigate back to Home page", async () => {
+    await headerPage.clickOnLogo();
+  });
+
+  await test.step("Mark a product as favorite", async () => {
+    await homePage.markProductAsFavorite("Sample Trouser Name");
+    const favButton = homePage.getStyledFavoriteButton("Sample Trouser Name");
+    await expect(favButton).toHaveCSS("color", "rgb(255, 0, 0)");
+  });
+
+  await test.step("Verify product appears in Favorites", async () => {
+    await headerPage.clickOnFavorite();
+    await expect(favoritesPage.getProducts()).toHaveCount(1);
+  });
+});
---
+++ tests/products/ProductDetailsTest.spec.js
@@ -0,0 +1,71 @@
+import { test, expect } from "@fixtures/fixtures";
+import testData from "@testdata/products.json";
+
+test("Verify product details", async ({
+  loggedInPage,
+  homePage,
+  productDetailsPage,
+  headerPage,
+}) => {
+  const { name, description, price } = testData.product[0];
+  await expect(loggedInPage).toHaveURL(/\/ecommerce$/);
+  await test.step("Select a product by name and click on it", async () => {
+    await homePage.clickOnProduct(name);
+  });
+  await test.step("Validate product details", async () => {
+    await loggedInPage.waitForTimeout(3000);
+    await expect(productDetailsPage.getProductName()).toHaveText(name);
+    await expect(productDetailsPage.getProductDescription()).toHaveText(
+      description,
+    );
+    await expect(productDetailsPage.getProductPrice()).toHaveText(price);
+  });
+  await test.step("Navigate back to products page", async () => {
+    await productDetailsPage.clickOnBackToProducts();
+    await expect(loggedInPage).toHaveURL(/\/ecommerce$/);
+  });
+  await test.step("Logout from the application", async () => {
+    await headerPage.logout();
+  });
+});
+
+test("Verify product details-1", async ({
+  loggedInPage,
+  homePage,
+  productDetailsPage,
+  headerPage,
+}) => {
+  const { name, description, price } = testData.product[0];
+  await expect(loggedInPage).toHaveURL(/\/ecommerce$/);
+  await test.step("Select a product by name and click on it", async () => {
+    await homePage.clickOnProduct(name);
+  });
+  await test.step("Validate product details", async () => {
+    await loggedInPage.waitForTimeout(3000);
+    await expect(productDetailsPage.getProductName()).toHaveText(name);
+    await expect(productDetailsPage.getProductDescription()).toHaveText(
+      description,
+    );
+    await expect(productDetailsPage.getProductPrice()).toHaveText(price);
+  });
+  await test.step("Increase product quantity and validate", async () => {
+    await productDetailsPage.increaseProductQuantity();
+    await expect(productDetailsPage.getProductQuantity()).toHaveValue("2");
+  });
+  await test.step("Add product to cart", async () => {
+    await productDetailsPage.addProductToCart();
+  });
+  // await productDetailsPage.decreaseProductQuantity();
+  // await expect(productDetailsPage.getProductQuantity()).toHaveValue("1");
+  await test.step("Navigate back to products page", async () => {
+    await productDetailsPage.clickOnBackToProducts();
+    await expect(loggedInPage).toHaveURL(/\/ecommerce$/);
+  });
+  await test.step("Click on cart button", async () => {
+    await headerPage.clickOnCartButton();
+    await loggedInPage.waitForTimeout(3000);
+  });
+  await test.step("Logout from the application", async () => {
+    await headerPage.logout();
+  });
+});
