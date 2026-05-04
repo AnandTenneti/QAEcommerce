2f1cd4c5b591b3b0817d8a7340f489e4274eedc7
AnnadTenneti <tenneti.anand@gmail.com>
2026-05-04 15:39:50 +0530

    Update LoginTest

---
+++ tests/login/LoginTest.spec.js
@@ -0,0 +1,44 @@
+import { test, expect } from "@fixtures/fixtures";
+import testData from "@testdata/testData_1.json";
+import { errorMessages } from "@constants/errorMessages";
+
+test.describe("Login Tests", () => {
+  test("[@smoke] Login with valid credentials", async ({
+    loggedInPage,
+    headerPage,
+  }) => {
+    await expect(loggedInPage).toHaveURL(/\/ecommerce$/);
+
+    await headerPage.logout();
+
+    await expect(loggedInPage).toHaveURL(/\/ecommerce\/login$/);
+  });
+
+  test.describe("[@regression] Invalid Login Scenarios", () => {
+    const scenarios = [
+      {
+        name: "Invalid Password",
+        data: testData.invalidPassword[0],
+        expectedError: errorMessages.invalidPassword,
+      },
+      {
+        name: "Invalid Username",
+        data: testData.invalidUserName[0],
+        expectedError: errorMessages.invalidUsername,
+      },
+      {
+        name: "Invalid Username and Password",
+        data: testData.invalidUserNameAndPassword[0],
+        expectedError: errorMessages.invalidUsernameAndPassword,
+      },
+    ];
+    for (const scenario of scenarios) {
+      test(scenario.name, async ({ loginPage }) => {
+        await loginPage.goToApplicationLoginPage(/\/ecommerce\/login$/);
+        await loginPage.login(scenario.data.username, scenario.data.password);
+        const errors = await loginPage.getErrorMessages();
+        expect(errors).toContain(scenario.expectedError);
+      });
+    }
+  });
+});
