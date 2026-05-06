81587447332ff5a41b26d21f9e74c6c9048954ab
AnnadTenneti <tenneti.anand@gmail.com>
2026-05-06 12:54:20 +0530

    Updating LoginTest.spec.js

--- tests/login/LoginTest.spec.js
+++ tests/login/LoginTest.spec.js
@@ -1,12 +1,9 @@
-import { test, expect } from "@fixtures/fixtures";
+import { test, expect } from "@hooks/testHooks";
 import testData from "@testdata/testData_1.json";
 import { errorMessages } from "@constants/errorMessages";
 
 test.describe("Login Tests", () => {
-  test("[@smoke] Login with valid credentials", async ({
-    loggedInPage,
-    headerPage,
-  }) => {
+  test("[@smoke] Valid Login", async ({ loggedInPage, headerPage }) => {
     await expect(loggedInPage).toHaveURL(/\/ecommerce$/);
 
     await headerPage.logout();
@@ -14,31 +11,29 @@ test.describe("Login Tests", () => {
     await expect(loggedInPage).toHaveURL(/\/ecommerce\/login$/);
   });
 
-  test.describe("[@regression] Invalid Login Scenarios", () => {
-    const scenarios = [
-      {
-        name: "Invalid Password",
-        data: testData.invalidPassword[0],
-        expectedError: errorMessages.invalidPassword,
-      },
-      {
-        name: "Invalid Username",
-        data: testData.invalidUserName[0],
-        expectedError: errorMessages.invalidUsername,
-      },
-      {
-        name: "Invalid Username and Password",
-        data: testData.invalidUserNameAndPassword[0],
-        expectedError: errorMessages.invalidUsernameAndPassword,
-      },
-    ];
-    for (const scenario of scenarios) {
-      test(scenario.name, async ({ loginPage }) => {
-        await loginPage.goToApplicationLoginPage(/\/ecommerce\/login$/);
-        await loginPage.login(scenario.data.username, scenario.data.password);
-        const errors = await loginPage.getErrorMessages();
-        expect(errors).toContain(scenario.expectedError);
-      });
-    }
-  });
+  const scenarios = [
+    {
+      name: "Invalid Password",
+      data: testData.invalidPassword[0],
+      expectedError: errorMessages.invalidPassword,
+    },
+    {
+      name: "Invalid Username",
+      data: testData.invalidUserName[0],
+      expectedError: errorMessages.invalidUsername,
+    },
+    {
+      name: "Invalid Username and Password",
+      data: testData.invalidUserNameAndPassword[0],
+      expectedError: errorMessages.invalidUsernameAndPassword,
+    },
+  ];
+  for (const scenario of scenarios) {
+    test(`[@regression] ${scenario.name}`, async ({ loginPage }) => {
+      await loginPage.goToApplicationLoginPage(/\/ecommerce\/login$/);
+      await loginPage.login(scenario.data.username, scenario.data.password);
+      const errors = await loginPage.getErrorMessages();
+      expect(errors).toContain(scenario.expectedError);
+    });
+  }
 });
