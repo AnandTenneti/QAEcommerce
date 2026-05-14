5a9fe204824478d46d2bb2afb3eebdbfbc683308
AnnadTenneti <tenneti.anand@gmail.com>
2026-05-14 07:10:56 +0530

    Upload Github Pages report

--- tests/favorites/FavoritesTest.spec.js
+++ tests/favorites/FavoritesTest.spec.js
@@ -14,18 +14,29 @@ test("[@regression] Verify empty favorites state and product addition behavior",
     );
   });
 
+  await test.step("Refresh the page and verify empty state persists", async () => {
+    await loggedInPage.reload();
+    await expect(favoritesPage.getMessage()).toContainText(
+      "You have no favorite products",
+    );
+  });
+
   await test.step("Navigate back to Home page", async () => {
     await headerPage.clickOnLogo();
   });
 
   await test.step("Mark a product as favorite", async () => {
     await homePage.markProductAsFavorite("Sample Trouser Name");
-    const favButton = homePage.getStyledFavoriteButton("Sample Trouser Name");
+    const favButton = homePage.favoritedButton("Sample Trouser Name");
     await expect(favButton).toHaveCSS("color", "rgb(255, 0, 0)");
   });
 
   await test.step("Verify product appears in Favorites", async () => {
     await headerPage.clickOnFavorite();
+    await expect(favoritesPage.getProducts()).toHaveCount(1);
+  });
+  await test.step("Refresh the Favorites page and verify product is still present", async () => {
+    await loggedInPage.reload();
     await expect(favoritesPage.getProducts()).toHaveCount(1);
   });
 });
